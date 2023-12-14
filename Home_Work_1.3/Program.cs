1a63a2b723605a28aec42c8f1d402a29448948fa
Stepanov Nikolay <profy76794@gamil.com>
2023-12-14 19:55:28 +0300

    Example complited

--- Home_Work_1.3/Program.cs
+++ Home_Work_1.3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+Найдите количество элементов массива, значения которых лежат в отрезке [20,90]. */
+
+int[] array = {10, 25, 33, 91, 45, 46};
+int number = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 20 && array[i] < 90) {
+
+        number = number + 1;
+    }
+}
+
+Console.Write(number);
